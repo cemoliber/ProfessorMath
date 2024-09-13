77ed6b256f447cf9c76ff0f2a203c34e9bdca276
cemalettin cosgun <72857671+cemoliber@users.noreply.github.com>
2024-09-13 03:49:20 +0300

    Professor Math

---
+++ Math2 - Kopya (4)/app/src/main/java/com/example/math2/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.math2
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.gms.ads.*
+
+class MainActivity : AppCompatActivity() {
+    private companion object{
+        private const val TAG = "BANNER_AD_TAG"
+    }
+    private var adView:AdView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //initialize
+        MobileAds.initialize(this@MainActivity) {
+            Log.d(TAG,"onInitializationCompleted: ")
+        }
+
+
+        MobileAds.setRequestConfiguration(RequestConfiguration.Builder().setTestDeviceIds(
+            listOf("TEST_DEVICE1","TEST_DEVICE2")
+        ).build())
+
+        //init banner ad
+        adView = findViewById(R.id.bannerAd)
+        //Ad Request
+        val adRequest = AdRequest.Builder().build()
+        //load ad
+        adView?.loadAd(adRequest)
+
+        //setup ad callbacklisteners
+        adView?.adListener = object : AdListener(){
+
+            override fun onAdClosed() {
+                super.onAdClosed()
+                Log.d(TAG,"onAdClosed: ")
+            }
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                super.onAdFailedToLoad(adError)
+                Log.e(TAG,"onAdFailedToLoad: ${adError.message}")
+            }
+
+            override fun onAdOpened() {
+                super.onAdOpened()
+                Log.d(TAG,"onAdOpened: ")
+            }
+
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+                Log.d(TAG,"onAdLoaded: ")
+            }
+
+            override fun onAdClicked() {
+                super.onAdClicked()
+                Log.d(TAG,"onAdClicked: ")
+            }
+
+            override fun onAdImpression() {
+                super.onAdImpression()
+                Log.d(TAG,"onAdImpression: ")
+            }
+
+        }
+
+        selectLevel()
+
+    }
+
+    override fun onPause() {
+        adView?.pause()
+        super.onPause()
+        Log.d(TAG,"onPause: ")
+    }
+
+    override fun onResume() {
+        adView?.resume()
+        super.onResume()
+        Log.d(TAG,"onResume: ")
+    }
+
+    override fun onDestroy() {
+        adView?.destroy()
+        super.onDestroy()
+        Log.d(TAG,"onDestroy: ")
+    }
+
+    private fun selectLevel() {
+        val startButton = findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+            var jump2 = MediaPlayer.create(this@MainActivity, R.raw.jump2)
+            jump2.start()
+            // Hedef aktiviteyi başlat
+            val intent = Intent(this, Operations::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ Math2 - Kopya (4)/app/src/main/java/com/example/math2/Operations.kt
@@ -0,0 +1,131 @@
+package com.example.math2
+
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import com.google.android.gms.ads.*
+
+class Operations : AppCompatActivity() {
+
+    private companion object{
+        private const val TAG = "BANNER_AD_TAG"
+    }
+    private var adView: AdView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operations)
+
+        var jump2 = MediaPlayer.create(this@Operations, R.raw.jump2)
+
+        val getSpinnerLevel: Spinner = findViewById(R.id.levelSpinner)
+
+        val levels = arrayOf("Seviye 1", "Seviye 2", "Seviye 3")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, levels)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        getSpinnerLevel.adapter = adapter
+
+        getSpinnerLevel.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+                val getSpinnerData = levels[position]
+
+                val sumImg = findViewById<ImageView>(R.id.addition)
+
+                sumImg.setOnClickListener {
+                    jump2.start()
+                    val intent = Intent(this@Operations, SumOperation::class.java)
+                    intent.putExtra("setSpinnerData", getSpinnerData)
+                    startActivity(intent)
+
+                }
+
+                val subImg = findViewById<ImageView>(R.id.subtraction)
+
+                subImg.setOnClickListener{
+                    jump2.start()
+                    val intent = Intent(this@Operations,SubOperation::class.java)
+                    intent.putExtra("setSpinnerData",getSpinnerData)
+                    startActivity(intent)
+                }
+
+                val multiplyImg = findViewById<ImageView>(R.id.multiply)
+
+                multiplyImg.setOnClickListener{
+                    jump2.start()
+                    val intent = Intent(this@Operations,MultiOperation::class.java)
+                    intent.putExtra("setSpinnerData",getSpinnerData)
+                    startActivity(intent)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        //initialize
+        MobileAds.initialize(this@Operations) {
+            Log.d(TAG, "onInitializationCompleted: ")
+        }
+
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder().setTestDeviceIds(
+                listOf("TEST_DEVICE1", "TEST_DEVICE2")
+            ).build()
+        )
+
+        //init banner ad
+        adView = findViewById(R.id.bannerAd3)
+        //Ad Request
+        val adRequest = AdRequest.Builder().build()
+        //load ad
+        adView?.loadAd(adRequest)
+
+        //setup ad callbacklisteners
+        adView?.adListener = object : AdListener() {
+
+            override fun onAdClosed() {
+                super.onAdClosed()
+                Log.d(TAG, "onAdClosed: ")
+            }
+
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                super.onAdFailedToLoad(adError)
+                Log.e(TAG, "onAdFailedToLoad: ${adError.message}")
+            }
+
+            override fun onAdOpened() {
+                super.onAdOpened()
+                Log.d(TAG, "onAdOpened: ")
+            }
+
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+                Log.d(TAG, "onAdLoaded: ")
+            }
+
+            override fun onAdClicked() {
+                super.onAdClicked()
+                Log.d(TAG, "onAdClicked: ")
+            }
+
+            override fun onAdImpression() {
+                super.onAdImpression()
+                Log.d(TAG, "onAdImpression: ")
+            }
+
+        }
+    }
+}
